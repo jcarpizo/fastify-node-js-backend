c30e82ae6d9834d24c8b4bbc29b5ca7f3f9184cb
Jasper Carpizo <jasper@incube8.sg>
2025-07-13 23:24:12 +0800

    feat: remove api key

--- src/middlewares/api-key-guard.ts
+++ src/middlewares/api-key-guard.ts
@@ -2,7 +2,7 @@ import {FastifyRequest, FastifyReply} from 'fastify'
 
 export async function verifyApiKey(request: FastifyRequest, reply: FastifyReply) {
     const apiKey = request.headers['x-api-key']
-    const validKeys = new Set(['3qohBUb4RJLUduNQ2ArCrokddmtmckl42vZ1g0IN'])
+    const validKeys = new Set([process.env.API_KEY])
 
     if (!apiKey || !validKeys.has(apiKey as string)) {
         reply.code(401).send({error: 'Unauthorized: Invalid API key'})
